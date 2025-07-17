e7ecd7f4edb81e841b2c5ec67debd57464e837ce
Khairi Reda <lio889@gmail.com>
2025-07-17 15:27:53 -0400

    first commit

---
+++ controller.js
@@ -0,0 +1,410 @@
+// 800 miliseconds of fixation
+var FIXATION_TIME = 800;
+
+function BlockController(options)
+{
+    this.options = options;
+    this.data = [];
+    this.mode = options.mode || 'mean';  // 'mean' or 'std'
+    this.stepSize = options.stepSize || 0.02;
+    this.minDelta = options.minDelta || 0.001;
+    this.maxDelta = options.maxDelta || 1.0;
+    this.initialDelta = options.initialDelta || 0.3;
+    this.classNum = options.classNum || 10;
+
+    // how many trials so far
+    this.trialCount = options.trialCount || 0;
+
+    this.width = options.width || 100;
+    this.height = options.height || 50;
+
+    // User-defined callback when a placeholder is clicked
+    this.onSelect = options.onSelect || function(selectedIndex) {
+        console.warn("No onSelect handler defined. Selected:", selectedIndex);
+    };
+
+    this.delta = this.initialDelta;
+    this.reversals = 0;
+    this.lastDirection = null;
+
+    this.stimPair = new StimulusPair(this.classNum);
+    this.generateTrial();
+
+    (function(object, _options) {
+    d3.select(document)
+        .on('keydown', function() {
+             if (d3.event.keyCode === 13)
+             {
+                 // enter, advance to next trial
+                 var result = object.recordSelection();
+                 if (result === null) {
+                     // no selection, do nothing
+                 }
+                 else {
+                     var done = object.nextTrial(result);
+                     if (done)
+                     {
+                         console.log('block complete');
+                         if (_options.onBlockEnd) {
+                             _options.onBlockEnd();
+                         }
+                     }
+                 }
+             }
+        })
+    })(this, options)
+}
+
+BlockController.prototype.generateTrial = function()
+{
+
+    this.correct = undefined;
+    this.curTrial = {
+        mode: this.mode,
+        requestedDelta: this.delta,
+        delta: 0,
+        deltaSecondary: 0,
+        correct: undefined,
+        trialNum: this.data.length+1,
+        generationTime: Date.now()
+    };
+
+    if (this.mode === 'mean') {
+        this.stimPair.optimize(this.delta, undefined);
+        if (this.stimPair.stim1.mean > this.stimPair.stim2.mean) {
+            this.correct = 1;
+        }
+        else {
+            this.correct = 2;
+        }
+        this.curTrial.delta = Math.abs(this.stimPair.stim1.mean-this.stimPair.stim2.mean);
+        this.curTrial.deltaSecondary = Math.abs(this.stimPair.stim1.std-this.stimPair.stim2.std);
+
+    } else {
+        this.stimPair.optimize(undefined, this.delta);
+        if (this.stimPair.stim1.std > this.stimPair.stim2.std) {
+            this.correct = 1;
+        }
+        else {
+            this.correct = 2;
+        }
+        this.curTrial.delta = Math.abs(this.stimPair.stim1.std-this.stimPair.stim2.std);
+        this.curTrial.deltaSecondary = Math.abs(this.stimPair.stim1.mean-this.stimPair.stim2.mean);
+    }
+    this.curTrial.generationTime = Date.now() - this.curTrial.generationTime;
+    this.curTrial.fixationTime = FIXATION_TIME;
+
+    if (FIXATION_TIME)
+    {
+        // show cross
+        d3.select("#fixationCross").style('visibility', 'visible');
+
+        (function(_this) {
+            setTimeout(function() {
+                _this.showTrial();
+                d3.select("#fixationCross").style('visibility', 'hidden');
+            }, FIXATION_TIME);
+        })(this);
+    }
+    else {
+        this.showTrial();
+    }
+};
+
+BlockController.prototype.nextTrial = function(isCorrect)
+{
+    // clear the last trial
+    this.clearTrial();
+
+    const oldDelta = this.delta;
+    let direction;
+
+    if (isCorrect)
+    {
+        // one down
+        this.delta = Math.max(this.minDelta, this.delta - this.stepSize);
+        direction = 'down';
+    } else
+    {
+        // three up
+        this.delta = Math.min(this.maxDelta, this.delta + 3 * this.stepSize);
+        direction = 'up';
+    }
+
+    if (this.lastDirection && direction !== this.lastDirection) {
+        this.reversals++;
+    }
+
+    this.lastDirection = direction;
+    if (this.trialCount > 0 && this.data.length >= this.trialCount) {
+        return true;
+    }
+    else {
+        this.generateTrial();
+        return false;
+    }
+};
+
+BlockController.prototype.getStimulusPair = function() {
+    return this.stimPair;
+};
+
+BlockController.prototype.getCurrentDelta = function() {
+    return this.delta;
+};
+
+BlockController.prototype.getReversalCount = function() {
+    return this.reversals;
+};
+
+BlockController.prototype.clearTrial = function() {
+    const stim1Group = d3.select("#stimulusGroup1");
+    const stim2Group = d3.select("#stimulusGroup2");
+
+    stim1Group.selectAll("*").remove();
+    stim2Group.selectAll("*").remove();
+}
+
+BlockController.prototype.showTrial = function(options)
+{
+    this.selected = undefined;
+    if (!options) { options = this.options; }
+
+    const duration = options?.exposureTime || 0;
+    const visType = options?.visType || VisType.VIS_BARS;
+
+    this.curTrial.exposureTime = duration;
+    this.curTrial.visType = visType;
+
+    const stim1Group = d3.select("#stimulusGroup1");
+    const stim2Group = d3.select("#stimulusGroup2");
+
+    stim1Group.selectAll("*").remove();
+    stim2Group.selectAll("*").remove();
+
+    this.stimPair.plotPair(
+        stim1Group,
+        stim2Group,
+        this.width,
+        this.height,
+        visType
+    );
+
+    (function(_this, _options, _stim1Group, _stim2Group, _duration)
+    {
+
+        _stim1Group.select("rect.selector")
+            .on('mousedown', function()
+            {
+                _stim1Group.select('rect.selector')
+                    .style('stroke', 'red')
+                    .classed('activeSelector', true);
+                _stim2Group.select('rect.selector')
+                    .style('stroke', null)
+                    .classed('activeSelector', false);
+                _this.selected = 1;
+            });
+        _stim2Group.select("rect.selector")
+            .on('mousedown', function() {
+                _stim1Group.select('rect.selector')
+                    .style('stroke', null)
+                    .classed('activeSelector', false);
+                _stim2Group.select('rect.selector')
+                    .style('stroke', 'red')
+                    .classed('activeSelector', true);
+                _this.selected = 2;
+            });
+
+        if (_duration > 0)
+        {
+            _this.timeout = setTimeout(() => {
+                /*
+                _this.maskStimulus(_stim1Group, 0);
+                _this.maskStimulus(_stim2Group, 1);
+                */
+                _this.maskStimulus();
+
+                if (typeof _options.onStimulusHidden === 'function') {
+                    options.onStimulusHidden();
+                }
+                _this.timeout = undefined;
+            }, _options.exposureTime);
+        }
+
+    })(this, options, stim1Group, stim2Group, duration);
+    this.displayTime = Date.now();
+
+};
+
+BlockController.prototype.recordSelection = function()
+{
+
+
+    if (this.selected)
+    {
+        // remove any timeout
+        if (this.timeout !== undefined) {
+            clearTimeout(this.timeout);
+            this.timeout = undefined;
+        }
+
+        var isCorrect = this.selected == this.correct;
+        if (isCorrect) {
+            console.log('Correct!');
+        }
+        else {
+            console.log('Incorrect');
+        }
+        this.curTrial.correct = isCorrect ? 1 : 0;
+        this.curTrial.selection = this.selected == 1 ? 'left' : 'right' ;
+        this.curTrial.responseTime = Date.now() - this.displayTime;
+        this.data.push(this.curTrial);
+
+        this.selected = undefined;
+
+        return isCorrect;
+
+    }
+    else {
+        return null;
+    }
+}
+
+BlockController.prototype.maskStimulus = function()
+{
+    const self = this;
+    const stim1Group = d3.select("#stimulusGroup1");
+    const stim2Group = d3.select("#stimulusGroup2");
+    stim1Group.select(".visGroup").style('visibility', 'hidden');
+    stim2Group.select(".visGroup").style('visibility', 'hidden');
+};
+
+function ExperimentControl(blockConfigs, svg, w, h, gap, options = {}) {
+    this.svg = svg;
+    this.width = w;
+    this.height = h;
+    this.gap = gap;
+
+    this.data = [];
+    this.blockConfigs = blockConfigs;
+    this.currentIndex = -1;
+    this.currentBlock = null;
+
+    // Callbacks
+    this.onBlockStart = function(index, config) {};
+    this.onBlockEnd = function(index, summary) {};
+    this.onExperimentEnd = function() {};
+    this.onTrialStart = function() {};
+    this.onTrialEnd = function() {};
+
+    // Break settings
+    this.breakEveryKBlocks = BREAK_EVERY; // default every 2 blocks
+    this.breakMessage = options.breakMessage ?? "Take a short break!";
+}
+
+// break every k blocks
+var BREAK_EVERY = 0;
+
+ExperimentControl.prototype.start = function() {
+    var CROSS_SIZE = 10;
+    var CROSS_THICKNESS = 3;
+
+    if (this.svg) {
+        d3.select("#stimulusGroup1").remove();
+        d3.select("#stimulusGroup2").remove();
+        d3.select("#fixationCross").remove();
+
+        var g1 = this.svg.append('g')
+            .attr('id', 'stimulusGroup1')
+            .attr('transform', 'translate(0,0)');
+
+        var g2 = this.svg.append('g')
+            .attr('id', 'stimulusGroup2')
+            .attr('transform', 'translate(' + (this.width + this.gap) + ',0)');
+
+        var gFix = this.svg.append('g')
+            .style('visibility', 'hidden')
+            .attr('id', 'fixationCross')
+            .attr('transform', 'translate(' + (this.width + this.gap / 2) + ',' + (this.height / 2) + ')');
+
+        gFix.append('line')
+            .attr('y1', -CROSS_SIZE / 2).attr('y2', CROSS_SIZE / 2)
+            .style('stroke', 'black').style('stroke-width', CROSS_THICKNESS);
+
+        gFix.append('line')
+            .attr('x1', -CROSS_SIZE / 2).attr('x2', CROSS_SIZE / 2)
+            .style('stroke', 'black').style('stroke-width', CROSS_THICKNESS);
+    }
+    this.nextBlock();
+};
+
+ExperimentControl.prototype.nextBlock = function(returnFromBreak) {
+
+    if (!returnFromBreak) {
+        this.currentIndex++;
+    }
+
+    if (this.currentIndex >= this.blockConfigs.length) {
+        this.onExperimentEnd();
+        return true;
+    }
+
+    // Check if we need a break (but not before the first block)
+    if (
+        !returnFromBreak &&
+        this.breakEveryKBlocks > 0 &&
+        this.currentIndex > 0 &&
+        this.currentIndex % this.breakEveryKBlocks === 0
+    ) {
+        const remaining = this.blockConfigs.length - this.currentIndex;
+        this.showBreakModal(remaining);
+        return false; // stop until user resumes
+    }
+
+    if (this.onBlockStart) {
+        this.onBlockStart(this.currentIndex, this.blockConfigs[this.currentIndex]);
+    }
+
+    const config = this.blockConfigs[this.currentIndex];
+    const self = this;
+
+    this.currentBlock = new BlockController({
+        width: this.width,
+        height: this.height,
+        ...config,
+        onBlockEnd: function() {
+            self.storeBlockResponses();
+            var done = self.nextBlock();
+        }
+    });
+
+    return false;
+};
+
+ExperimentControl.prototype.showBreakModal = function(remainingBlocks) {
+    const modal = document.getElementById("breakModal");
+    const message = document.getElementById("breakMessage");
+    const button = document.getElementById("continueButton");
+
+    message.innerHTML =
+        this.breakMessage +
+        "<br><br>You have <b>" +
+        remainingBlocks +
+        "</b> blocks remaining.";
+
+    modal.style.display = "flex";
+
+    button.onclick = () => {
+        modal.style.display = "none";
+        console.log('resuming blocks');
+        this.nextBlock(true); // Resume the experiment
+    };
+};
+
+ExperimentControl.prototype.storeBlockResponses = function(index) {
+    var blockData = this.currentBlock.data;
+    for (var i = 0; i < blockData.length; i++) {
+        blockData[i].blockNum = this.currentIndex + 1;
+        this.data.push(blockData[i]);
+    }
+};
---
+++ visspeed.js
@@ -0,0 +1,371 @@
+
+var MIN_D = 0;
+var MAX_D = 1;
+var PETRUB = Math.abs(MAX_D-MIN_D) / 50;
+
+const VisType = Object.freeze({
+    VIS_BARS: 'bar',
+    VIS_LINES: 'line',
+    VIS_AREA: 'area',
+});
+
+function VisSpeedStim(classNum)
+{
+    this.yScale = d3.scaleLinear().domain([0, 1]).range([0, 1]);
+    this.classNum = classNum;
+    this.random();
+}
+
+VisSpeedStim.prototype.random = function()
+{
+    this.data = [];
+    for (var i=0; i<this.classNum; i++) {
+        var r = Math.random() * (MAX_D-MIN_D) + MIN_D;
+        this.data.push(r);
+    }
+    this.findMinMax(true);
+    this.computeStats();
+}
+
+VisSpeedStim.prototype.findMinMax = function(setScale)
+{
+    var minD = Number.MAX_VALUE;
+    var maxD = -Number.MAX_VALUE;
+    for (var i=0; i<this.data.length; i++) {
+        var d = this.data[i];
+        minD = Math.min(minD, d);
+        maxD = Math.max(maxD, d);
+    }
+    this.minD = minD;
+    this.maxD = maxD;
+    if (setScale) {
+        this.setYScale(minD, maxD);
+    }
+    return [this.minD, this.maxD];
+}
+
+VisSpeedStim.prototype.setYScale = function(minY, maxY)
+{
+    this.yScale.domain([minY, maxY]);
+}
+
+VisSpeedStim.prototype.render = function(g, w, h, visType)
+{
+    g.selectAll('*').remove();
+
+    var PADDING = 7;
+
+    // render a square that is slightly larger than the vis space
+    g.append('rect')
+        .attr('class', 'selector')
+        .style('stroke', 'black')
+        .style('fill', 'white')
+        .attr('x', -PADDING)
+        .attr('y', -PADDING)
+        .attr('width', w+PADDING*2)
+        .attr('height', h+PADDING*2);
+
+    var gg = g.append('g')
+        .attr('class', 'visGroup')
+        .style('pointer-events', 'none');
+
+    switch(visType)
+    {
+    case VisType.VIS_BARS:
+        this.renderBars(gg, w, h);
+        break;
+    case VisType.VIS_LINES:
+        this.renderLines(gg, w, h);
+        break;
+    case VisType.VIS_AREA:
+        this.renderArea(gg, w, h);
+        break;
+    }
+}
+VisSpeedStim.prototype.renderLines = function(g, w, h)
+{
+    var xScale = d3.scaleLinear()
+        .domain([0, this.data.length - 1])
+        .range([0, w]);
+
+    var yScale = d3.scaleLinear()
+        .domain(this.yScale.domain())  // domain already set by setYScale
+        .range([h, 0]);  // SVG y is inverted: 0 is top
+
+    var lineGen = d3.line()
+        .x((d, i) => xScale(i))
+        .y((d) => yScale(d))
+        .curve(d3.curveLinear);  // you can try d3.curveLinear, d3.curveBasis, etc.
+
+    var path = g.selectAll('path.curve')
+        .data([this.data]);  // single path for the whole dataset
+
+    path.enter()
+        .append('path')
+        .attr('class', 'curve')
+        .merge(path)
+        .attr('d', lineGen)
+        .attr('fill', 'none')
+        .attr('stroke', 'black')
+        .attr('stroke-width', 2);
+
+    path.exit().remove();
+}
+
+VisSpeedStim.prototype.renderArea = function(g, w, h) {
+    var xScale = d3.scaleLinear()
+        .domain([0, this.data.length - 1])
+        .range([0, w]);
+
+    var yScale = d3.scaleLinear()
+        .domain(this.yScale.domain())  // same domain as the original
+        .range([h, 0]);  // SVG y axis inverted
+
+    var areaGen = d3.area()
+        .x((d, i) => xScale(i))
+        .y0(h)  // baseline (bottom of chart)
+        .y1((d) => yScale(d))
+        .curve(d3.curveLinear);  // or curveLinear, curveBasis, etc.
+
+    var areaPath = g.selectAll('path.area')
+        .data([this.data]);
+
+    areaPath.enter()
+        .append('path')
+        .attr('class', 'area')
+        .merge(areaPath)
+        .attr('d', areaGen)
+        .attr('fill', '#69b3a2')
+        .attr('stroke', 'black')
+        .attr('stroke-width', 1);
+
+    areaPath.exit().remove();
+};
+
+VisSpeedStim.prototype.renderBars = function(g, w, h, gapW)
+{
+    var MIN_GAP = 2;
+    var MAX_TOTAL_GAP_RATIO = 0.05;
+
+    var gapCount = Math.max(0, this.classNum - 1);
+    var maxTotalGap = w * MAX_TOTAL_GAP_RATIO;
+
+    // Compute adaptive gap width
+    if (gapCount === 0) {
+        gapW = 0;
+    } else {
+        var idealGap = maxTotalGap / gapCount;
+        gapW = Math.max(MIN_GAP, idealGap);
+    }
+
+    // Compute bar width using total space left after gaps
+    var barW = (w - (gapCount * gapW)) / this.classNum;
+
+    var selection = g.selectAll('rect.bar').data(this.data);
+    selection = selection.enter()
+        .append('rect')
+        .merge(selection);
+
+    (function(_selection, _barW, _gapW, obj) {
+        _selection
+            .attr('class', 'bar')
+            .attr('x', function(d, i) {
+                return i*(_barW+_gapW);
+            })
+            .attr('y', function(d) {
+                var y = h*obj.yScale(d);
+                return h-y;
+            })
+            .attr('width', _barW)
+            .attr('height', function(d) {
+                return h*obj.yScale(d);
+            })
+    })(selection, barW, gapW, this);
+}
+
+VisSpeedStim.prototype.perturb = function()
+{
+    var done = false;
+    while (!done) {
+        // pick a random index to pertrub
+        var index = Math.floor(Math.random() * this.classNum);
+        var d = this.data[index];
+
+        // random pertrubation value
+        var r =PETRUB * ((Math.random() * 2)-1);
+        var newD = d + r;
+        if (newD >= MIN_D && newD <= MAX_D)
+        {
+            this.perturbedIndex = index;
+            this.originalValue = d;
+            this.originalMean = this.mean;
+            this.originalStd = this.std;
+
+            this.data[index] = newD;
+            done = true;
+
+            this.computeStats();
+
+        }
+    }
+}
+
+VisSpeedStim.prototype.revert = function()
+{
+    if (this.perturbedIndex !== undefined)
+    {
+        this.data[this.perturbedIndex] = this.originalValue;
+        this.perturbedIndex = undefined;
+        this.mean = this.originalMean;
+        this.std = this.originalStd;
+    }
+}
+
+VisSpeedStim.prototype.computeStats = function()
+{
+    this.mean = 0;
+    this.std = 0;
+    for (var i=0, len=this.data.length; i<len; i++ ) {
+        this.mean += this.data[i];
+    }
+    this.mean /= this.data.length;
+
+    for (var i=0, len=this.data.length; i<len; i++ ) {
+        this.std += Math.pow(this.data[i] - this.mean, 2);
+    }
+    this.std = Math.sqrt(this.std/(this.data.length-1));
+}
+
+function StimulusPair(classNum)
+{
+    this.classNum = classNum;
+    this.stim1 = new VisSpeedStim(classNum);
+    this.stim2 = new VisSpeedStim(classNum);
+}
+
+var MEAN_TOLERANCE = 0;
+var STD_TOLERANCE = 0;
+
+StimulusPair.prototype.plotPair = function(g1, g2, w, h, visType)
+{
+    var minMax1 = this.stim1.findMinMax();
+    var minMax2 = this.stim2.findMinMax();
+    var minMax = [Math.min(minMax1[0], minMax2[0]), Math.max(minMax1[1], minMax2[1]) ];
+    this.stim1.setYScale(minMax[0], minMax[1]);
+    this.stim2.setYScale(minMax[0], minMax[1]);
+
+    this.stim1.render(g1, w, h, visType);
+    this.stim2.render(g2, w, h, visType);
+}
+
+StimulusPair.prototype.optimize = function(deltaMean, deltaStd)
+{
+    var T_INIT = 1;
+    var T_END = 0.0001;     // end temperature
+    var ALPHA = 0.9;      // cooling rate
+    var ITER_COUNT = 5500;  // number of iterations per temp
+    var K = 2;
+
+    var t = T_INIT;
+
+    this.stim1 = new VisSpeedStim(this.classNum);
+    this.stim2 = new VisSpeedStim(this.classNum);
+
+    var s1 = this.stim1;
+    var s2 = this.stim2;
+
+    var solution = [s1, s2];
+    var solutionDiff, _solutionDiff;
+
+    if (deltaMean !== undefined) {
+        solutionDiff = Math.abs(Math.abs(s1.mean - s2.mean)-deltaMean);
+        _solutionDiff = Math.abs(s1.std-s2.std);
+    } else {
+        solutionDiff = Math.abs(Math.abs(s1.std - s2.std)-deltaStd);
+        _solutionDiff = Math.abs(s1.mean-s2.mean);
+    }
+    var cost = Number.MAX_VALUE;
+
+    while (t>=T_END)
+    {
+        var avgDiff = 0;
+        for (var iter=0; iter<ITER_COUNT; iter++)
+        {
+
+            // select stimulus to perturb
+            var index = Math.random() > 0.5 ? 1 : 0;
+            solution[index].perturb();
+
+            var diff, tolerance, _diff;
+
+            // test solution
+            if (deltaMean !== undefined)
+            {
+                // test the mean difference between the two solutions
+                var d = Math.abs(s1.mean - s2.mean);
+                avgDiff += d;
+                diff = Math.abs(d - deltaMean);
+                _diff = Math.abs(s1.std-s2.std);
+                tolernace = MEAN_TOLERANCE;
+            }
+            else {
+                var d = Math.abs(s1.std - s2.std)
+                avgDiff += d;
+                diff = Math.abs(d - deltaStd);
+                _diff = Math.abs(s1.mean-s2.mean);
+                tolerance = STD_TOLERANCE;
+            }
+
+            var better = false;
+            if (false && solutionDiff <= tolerance)
+            {
+                // current solution is satisfactory
+                if (diff <= tolerance)
+                {
+                    // so is new solution
+                    // see if it's worth adopting the new solution
+                    cost = _diff - _solutionDiff;
+
+                    // adopt new solution if cost on seconday measure is better
+                    better = cost < 0 ? true : false;
+                }
+            }
+            else {
+                cost = diff - solutionDiff +
+                    // 50 percent influence of other dimension
+                    (_diff - _solutionDiff) * .5;
+
+                if (cost <= 0) {
+                    // new solution is less costly
+                    better = true;
+                }
+                // current solution is not satisfactory
+                /*
+                if (diff < solutionDiff) {
+                    better = true;
+                }
+                else {
+                    cost =
+                        diff - solutionDiff +
+                        // 50 percent influence of other dimension
+                        (_diff - _solutionDiff) * .5;
+                }
+                */
+            }
+
+            var e = 1/(1+Math.exp(K*cost/t));
+            if (better  || Math.random() < e )
+            {
+                // take the new solution
+                solutionDiff = diff;
+                _solutionDiff = _diff;
+            }
+            else {
+                solution[index].revert();
+            }
+
+        }
+        //console.log('t: ' + t + ', solDiff: ' + (solutionDiff) + ', 2nd: ' + _solutionDiff);
+        t *= ALPHA;
+    }
+}
